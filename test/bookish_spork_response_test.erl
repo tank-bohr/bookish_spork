2b270899b6eea85afb19cc46dc5392be7225fc52
Alexey Nikitin <nikitin@bookmate.com>
2018-08-03 20:35:07 +0300

    Refactoring to increase code coverage (#10)
    - convert pattern-matching to picking from data structures
    - add bookish_spork_request:header/2 fun for one header extraction
    - separate versions of utc_now/0 func for tests 🤢

--- test/bookish_spork_response_test.erl
+++ test/bookish_spork_response_test.erl
@@ -1,14 +1,11 @@
 -module(bookish_spork_response_test).
 -include_lib("eunit/include/eunit.hrl").
 
--define(T, bookish_spork_response).
--define(NOW, {{2018, 4, 28}, {5, 51, 50}}).
-
 status_line_test() ->
-    ?assertEqual(<<"HTTP/1.1 204 No Content">>, ?T:status_line(204)).
+    ?assertEqual(<<"HTTP/1.1 204 No Content">>, bookish_spork_response:status_line(204)).
 
 response_headers_test() ->
-    Actual = ?T:headers(#{}, <<"XXX">>, ?NOW),
+    Actual = bookish_spork_response:headers(#{}, <<"XXX">>),
     Expected = <<
         "Content-Length: 3\r\n",
         "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
@@ -16,19 +13,52 @@ response_headers_test() ->
     >>,
     ?assertEqual(Expected, Actual).
 
-write_str_test() ->
-    Response = ?T:new(200, #{
-        <<"Content-Type">> => <<"text/plain">>,
-        <<"Date">> => <<"Tue, 12 Jun 2018 14:12:37 GMT">>
-    }, <<"Hello">>),
-    Expected = <<
-        "HTTP/1.1 200 OK\r\n",
-        "Content-Length: 5\r\n",
-        "Content-Type: text/plain\r\n",
-        "Date: Tue, 12 Jun 2018 14:12:37 GMT\r\n",
-        "Server: BookishSpork/0.0.1\r\n"
-        "\r\n",
-        "Hello"
-    >>,
-    Actual = ?T:write_str(Response),
-    ?assertEqual(Expected, Actual).
+write_str_test_() ->
+    DefaultResponse = bookish_spork_response:new(),
+    CustomsStatus = bookish_spork_response:new(502),
+    CustomsStatusAndContent = bookish_spork_response:new(409, <<"Booookish">>),
+    CustomsStatusAndHeaders = bookish_spork_response:new(307,
+        #{<<"Location">> => <<"https://example.com">>}),
+    CustomResponse = bookish_spork_response:new(200,
+        #{<<"Content-Type">> => <<"text/plain">>}, <<"Hello">>),
+    [
+        ?_assertEqual(<<
+            "HTTP/1.1 204 No Content\r\n",
+            "Content-Length: 0\r\n",
+            "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
+            "Server: BookishSpork/0.0.1\r\n",
+            "\r\n"
+        >>, bookish_spork_response:write_str(DefaultResponse)),
+        ?_assertEqual(<<
+            "HTTP/1.1 502 Bad Gateway\r\n",
+            "Content-Length: 0\r\n",
+            "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
+            "Server: BookishSpork/0.0.1\r\n"
+            "\r\n"
+        >>, bookish_spork_response:write_str(CustomsStatus)),
+        ?_assertEqual(<<
+            "HTTP/1.1 409 Conflict\r\n",
+            "Content-Length: 9\r\n",
+            "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
+            "Server: BookishSpork/0.0.1\r\n"
+            "\r\n",
+            "Booookish"
+        >>, bookish_spork_response:write_str(CustomsStatusAndContent)),
+        ?_assertEqual(<<
+            "HTTP/1.1 307 Temporary Redirect\r\n",
+            "Content-Length: 0\r\n",
+            "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
+            "Location: https://example.com\r\n"
+            "Server: BookishSpork/0.0.1\r\n"
+            "\r\n"
+        >>, bookish_spork_response:write_str(CustomsStatusAndHeaders)),
+        ?_assertEqual(<<
+            "HTTP/1.1 200 OK\r\n",
+            "Content-Length: 5\r\n",
+            "Content-Type: text/plain\r\n",
+            "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
+            "Server: BookishSpork/0.0.1\r\n"
+            "\r\n",
+            "Hello"
+        >>, bookish_spork_response:write_str(CustomResponse))
+    ].
