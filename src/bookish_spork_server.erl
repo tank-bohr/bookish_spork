54368cfa66466bbc2bd5cd5f841d9c36c3a03523
Alexey Nikitin <nikitin@bookmate.com>
2018-08-07 17:36:12 +0300

    Get rid of conditional time function for tests. Fixes #12

--- src/bookish_spork_server.erl
+++ src/bookish_spork_server.erl
@@ -121,5 +121,5 @@ read_body(Socket, ContentLength) ->
 
 %% @private
 reply(Socket, Response) ->
-    String = bookish_spork_response:write_str(Response),
+    String = bookish_spork_response:write_str(Response, calendar:universal_time()),
     gen_tcp:send(Socket, [String]).
--- test/bookish_spork_response_test.erl
+++ test/bookish_spork_response_test.erl
@@ -1,11 +1,13 @@
 -module(bookish_spork_response_test).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(NOW, {{2018, 4, 28}, {5, 51, 50}}).
+
 status_line_test() ->
     ?assertEqual(<<"HTTP/1.1 204 No Content">>, bookish_spork_response:status_line(204)).
 
 response_headers_test() ->
-    Actual = bookish_spork_response:headers(#{}, <<"XXX">>),
+    Actual = bookish_spork_response:headers(#{}, <<"XXX">>, ?NOW),
     Expected = <<
         "Content-Length: 3\r\n",
         "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
@@ -28,14 +30,14 @@ write_str_test_() ->
             "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
             "Server: BookishSpork/0.0.1\r\n",
             "\r\n"
-        >>, bookish_spork_response:write_str(DefaultResponse)),
+        >>, bookish_spork_response:write_str(DefaultResponse, ?NOW)),
         ?_assertEqual(<<
             "HTTP/1.1 502 Bad Gateway\r\n",
             "Content-Length: 0\r\n",
             "Date: Sat, 28 Apr 2018 05:51:50 GMT\r\n",
             "Server: BookishSpork/0.0.1\r\n"
             "\r\n"
-        >>, bookish_spork_response:write_str(CustomsStatus)),
+        >>, bookish_spork_response:write_str(CustomsStatus, ?NOW)),
         ?_assertEqual(<<
             "HTTP/1.1 409 Conflict\r\n",
             "Content-Length: 9\r\n",
@@ -43,7 +45,7 @@ write_str_test_() ->
             "Server: BookishSpork/0.0.1\r\n"
             "\r\n",
             "Booookish"
-        >>, bookish_spork_response:write_str(CustomsStatusAndContent)),
+        >>, bookish_spork_response:write_str(CustomsStatusAndContent, ?NOW)),
         ?_assertEqual(<<
             "HTTP/1.1 307 Temporary Redirect\r\n",
             "Content-Length: 0\r\n",
@@ -51,7 +53,7 @@ write_str_test_() ->
             "Location: https://example.com\r\n"
             "Server: BookishSpork/0.0.1\r\n"
             "\r\n"
-        >>, bookish_spork_response:write_str(CustomsStatusAndHeaders)),
+        >>, bookish_spork_response:write_str(CustomsStatusAndHeaders, ?NOW)),
         ?_assertEqual(<<
             "HTTP/1.1 200 OK\r\n",
             "Content-Length: 5\r\n",
@@ -60,5 +62,5 @@ write_str_test_() ->
             "Server: BookishSpork/0.0.1\r\n"
             "\r\n",
             "Hello"
-        >>, bookish_spork_response:write_str(CustomResponse))
+        >>, bookish_spork_response:write_str(CustomResponse, ?NOW))
     ].
