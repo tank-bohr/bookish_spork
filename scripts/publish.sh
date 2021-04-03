50b82cae4dcb9d04251b8445057b4bf80af90fd8
Alexey Nikitin <tank@bohr.su>
2021-04-03 18:52:48 +0300

    fix documentation publishing [ci skip]

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -ex
 
 ## Add the rebar3 hex plugin to global
 mkdir -p ~/.config/rebar3
-echo '{plugins, [{rebar3_hex, ">= 6.9.3"}]}.' > ~/.config/rebar3/rebar.config
+echo '{plugins, [{rebar3_hex, "6.11.2"}]}.' > ~/.config/rebar3/rebar.config
 
 rebar3 hex publish --yes --repo=hexpm
 rebar3 hex docs --repo=hexpm
